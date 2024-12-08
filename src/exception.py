dcf45ffc2be9a8ec60e0674455b35a9deafd36d3
Stojanka Tesanovic <stojankatesanovic@gmail.com>
2024-12-08 01:35:04 +0100

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -18,10 +18,10 @@ class CustomException(Exception):
         return self.error_message
 
 # TEST 
-if __name__ == "__main__":
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e, sys)
+# if __name__ == "__main__":
+#     try:
+#         a = 1/0
+#     except Exception as e:
+#         logging.info("Divide by Zero")
+#         raise CustomException(e, sys)
         
